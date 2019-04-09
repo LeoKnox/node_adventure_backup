461ac479b7cc00724da774b306a98c7bf6e15c67
LeoKnox <LeoKnox@users.noreply.github.com>
2019-04-08 22:35:40 -0700

    Add files via upload
    Added an object to message to be able to log sender.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(session({secret: "be very quiet"}))
 
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
+        console.log(msg)
         io.emit('chat message', msg)
     })
 })
